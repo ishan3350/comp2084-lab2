e56a53497ba2ca12dcbf6c37011d5b25c6fb163d
Ishankumar Patel <Ishanpatel3350@gmail.com>
2019-01-25 17:10:11 -0500

    Lab 2 completes here

---
+++ Lab2/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details(string Categories)
+        {
+            ViewBag.Categories = "You selected category: " + Categories;
+            return View();
+        }
+    }
+}
